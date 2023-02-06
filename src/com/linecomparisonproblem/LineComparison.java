a13ef46e7e6a9539f582ed7b823150fa27b2e74a
dbasumatary <basudhondip@gmail.com>
2023-02-06 18:02:41 +0530

    Added a method to check equality between two lines

--- src/com/linecomparisonproblem/LineComparison.java
+++ src/com/linecomparisonproblem/LineComparison.java
@@ -10,13 +10,30 @@ public class LineComparison {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         LineComparison length = new LineComparison();
-        double x1, y1, x2, y2;
+        double x1, y1, x2, y2, x3, y3, x4, y4;
+        System.out.println("Considering the first line");
         System.out.println("Enter x and y coordinates of the first point: ");
         x1 = input.nextDouble();
         y1 = input.nextDouble();
         System.out.println("Enter x and y coordinates of the second point: ");
         x2 = input.nextDouble();
         y2 = input.nextDouble();
-        System.out.println("The length of the line is " + length.lengthCalculate(x1,y1,x2,y2));
+        System.out.println("Considering the second line");
+        System.out.println("Enter x and y coordinates of the third point: ");
+        x3 = input.nextDouble();
+        y3 = input.nextDouble();
+        System.out.println("Enter x and y coordinates of the fourth point: ");
+        x4 = input.nextDouble();
+        y4 = input.nextDouble();
+        /*Storing the line length in two different variables */
+        Double firstLine = length.lengthCalculate(x1,y1,x2,y2);
+        Double secondLine = length.lengthCalculate(x3,y3,x4,y4);
+        /*Using equals method to check equality of two lengths*/
+        if(firstLine.equals(secondLine)){
+            System.out.println("The lines are equal");
+        }
+        else{
+            System.out.println("The lines are not equal");
+        }
     }
 }
